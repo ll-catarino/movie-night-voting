0f46e754d10468c37d9236ef846c350b13079645
ll-catarino <ll.catarino@campus.fct.unl.pt>
2021-07-30 10:01:43 +0100

    added dotenv dependency

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const express = require('express');
 const app = express();
 app.use(express.json()); // json parser middleware
@@ -5,7 +6,7 @@ app.use(express.json()); // json parser middleware
 //const cors = require('cors')
 //app.use(cors)
 
-const PORT = 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {console.log('api running on port ' + PORT)})
 
 const path = require('path');
